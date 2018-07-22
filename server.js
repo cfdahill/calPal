5cb09b53645f45c5caab0edf87f78a01e8d76984
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-07-22 09:57:14 -0700

    changed dirname

--- server.js
+++ server.js
@@ -58,11 +58,13 @@ app.use(passport.session()) // calls the deserializeUser
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static("client/build"));
-app.use('/static', express.static(path.join(__dirname, './client/build/static')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './client/build/'))
-    });
+app.use('/static', express.static(path.join(__dirname, 'public')))
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname, './client/build/'))
+    // });
 }
+
+// app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
 app.use('/auth', require('./routes/auth'))
